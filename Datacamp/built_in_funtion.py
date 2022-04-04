1d0c4e572a536fc43e07c613b487e66263eecb6c
soohyunme <soohyun.dev@gmail.com>
2022-04-04 16:07:59 +0900

    built in function

---
+++ Datacamp/built_in_funtion.py
@@ -0,0 +1,48 @@
+# range()
+
+# Create a range object that goes from 0 to 5
+nums = range(6)
+print(type(nums))
+
+# Convert nums to a list
+nums_list = list(nums)
+print(nums_list)
+
+# Create a new list of odd numbers from 1 to 11 by unpacking a range object
+nums_list2 = [*range(1, 12, 2)]
+print(nums_list2)
+
+# enumerate()
+
+names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
+
+# Rewrite the for loop to use enumerate
+indexed_names = []
+for i, name in enumerate(names):
+    index_name = (i, name)
+    indexed_names.append(index_name)
+print(indexed_names)
+
+# Rewrite the above for loop using list comprehension
+indexed_names_comp = [(i, name) for i, name in enumerate(names)]
+print(indexed_names_comp)
+
+# Unpack an enumerate object with a starting index of one
+indexed_names_unpack = [*enumerate(names, 1)]
+print(indexed_names_unpack)
+
+# map()
+
+names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
+
+# Use map to apply str.upper to each element in names
+names_map = map(str.upper, names)
+
+# Print the type of the names_map
+print(type(names_map))
+
+# Unpack names_map into a list
+names_uppercase = [*names_map]
+
+# Print the list created above
+print(names_uppercase)

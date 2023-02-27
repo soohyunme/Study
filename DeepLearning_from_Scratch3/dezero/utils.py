20ec2b1d29b5a19eaaee14e310d3edde2010481a
Soohyun-Kim <soohyun.dev@gmail.com>
2023-02-27 21:46:11 +0900

    2023-02-27

---
+++ DeepLearning_from_Scratch3/dezero/utils.py
@@ -0,0 +1,9 @@
+def _dot_var(v, verbose=False):
+    dot_var = '{} [label="{}", color=orange, style=filled]\n'
+
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        if v.name is not None:
+            name += ': '
+        name += str(v.shape) + ' ' + str(v.dtype)
+    return dot_var.format(id(v), name)
--- DeepLearning_from_Scratch3/steps/step14.py
+++ DeepLearning_from_Scratch3/steps/step14.py
@@ -1,24 +1,25 @@
-import unittest
 import numpy as np
-
 
 class Variable:
     def __init__(self, data):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
-
+        
         self.data = data
         self.grad = None
         self.creator = None
 
     def set_creator(self, func):
         self.creator = func
+    
+    def cleargrad(self):
+        self.grad = None
 
     def backward(self):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
-
+        
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
@@ -26,18 +27,15 @@ class Variable:
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs, )
-            
+
             for x, gx in zip(f.inputs, gxs):
-                x.grad = gx
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
 
                 if x.creator is not None:
                     funcs.append(x.creator)
-
-
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
 
 
 class Function:
@@ -47,98 +45,74 @@ class Function:
         if not isinstance(ys, tuple):
             ys = (ys, )
         outputs = [Variable(as_array(y)) for y in ys]
-
+        
         for output in outputs:
             output.set_creator(self)
         self.inputs = inputs
         self.outputs = outputs
-
+        
         return outputs if len(outputs) > 1 else outputs[0]
-
+    
     def forward(self, xs):
         raise NotImplementedError()
-
+    
     def backward(self, gys):
         raise NotImplementedError()
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 
 class Square(Function):
     def forward(self, x):
         return x ** 2
-
+    
     def backward(self, gy):
         x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
+    
+
+def square(x):
+    return Square()(x)
 
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
-
+    
     def backward(self, gy):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
-
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
-
-
-def square(x):
-    return Square()(x)
 
 
 def exp(x):
     return Exp()(x)
 
 
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-    
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-    
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-
 class Add(Function):
     def forward(self, x0, x1):
         y = x0 + x1
         return y
-
+    
     def backward(self, gy):
         return gy, gy
 
 
 def add(x0, x1):
     return Add()(x0, x1)
+    
 
+x = Variable(np.array(3.0))
+y = add(x, x)
+y.backward()
+print(x.grad)
 
 x = Variable(np.array(2.0))
-y = Variable(np.array(3.0))
-f1 = square(x)
-f2 = square(y)
-z = add(f1, f2)
-z.backward()
-print(z.data)
+y = add(add(x, x), x)
+y.backward()
 print(x.grad)
-print(y.grad)

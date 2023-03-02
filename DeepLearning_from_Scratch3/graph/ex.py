aea41b2e731245d6f61d500ad4d99bd6de0b7609
soohyun kim <soo@soohyuns-MacBook-Pro.local>
2023-03-02 17:05:44 +0900

    2023-03-02

--- DeepLearning_from_Scratch3/graph/ex.py
+++ DeepLearning_from_Scratch3/graph/ex.py
@@ -4,9 +4,16 @@ if '__file__' in globals():
 
 import numpy as np
 from dezero import Variable
-from dezero.utils import _dot_var
+from dezero.utils import _dot_var, _dot_func
 
 x = Variable(np.random.randn(2,3))
 x.name = 'x'
 print(_dot_var(x))
 print(_dot_var(x, verbose=True))
+
+
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(1.0))
+y = x0 + x1
+txt = _dot_func(y.creator)
+print(txt)
